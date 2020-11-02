5a76e14795b4a4083d8f6a6d3d36e6ad4c2a8bca
kchrisalis <kayecsmaranan@gmail.com>
2020-11-02 15:28:37 -0700

    things happened i think

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,11 +1,74 @@
 // Helper Functions 
 
-function newAcc() {
-  document.getElementById('loginTitle').innerHTML = "Create New Account";
-  document.getElementById("bTxt").innerHTML = `<p id="bTxt">Already have an account? Log in <a href="#" onclick="existAcc()">here.</a></p>`
+// Arrays
+let loginAccounts = initAcc();
+
+function initAcc() {
+  let storedlogAcc = localStorage.getItem('loginAcc');
+  if (storedlogAcc) {
+    return JSON.parse(storedlogAcc);
+  } else {
+    return [];
+  }
 }
 
-function existAcc() {
-  document.getElementById('loginTitle').innerHTML = "Log In";
-  document.getElementById("bTxt").innerHTML = `<p id="bTxt">Don't have an account? Make an account <a href="#" onclick="newAcc()">here.</a></p>`
+// Global Variables
+let user = document.getElementById("username");
+let pass = document.getElementById("password");
+let nUser = document.getElementById("newUser");
+let nPass = document.getElementById("newPass");
+
+// Clear Page
+function clearDivs() {
+  for (let i = 0; i < document.getElementById("content").children.length; i++) {
+    document.getElementById("content").children[i].style.display = "none";
+  }
+}
+
+// Display Div
+function displayDiv(divId) {
+  clearDivs();
+  document.getElementById(divId).style.display = "block";
+}
+
+// Log In
+function login () {
+
+}
+
+// Create Account
+function createLogin() {
+  // Check if username exists
+  if (check(loginAccounts, nUser, username) == true) {
+    alert('Taken username. Choose another one');
+  } else if (nUser.value == "" || nPass.value == "") {
+    alert("Please fill in all fields");
+  } else {
+    console.log(loginAccounts);
+    // Push account info to storage
+    loginAccounts.push({
+      username: nUser.value.toLowerCase(),
+      password: nPass.value
+    });
+
+    localStorage.setItem('loginAcc', JSON.stringify(loginAccounts));
+
+    // Clear
+    nUser.value = "";
+    nPass.value = "";
+
+    // Return to log in page
+    clearDivs();
+    displayDiv("logIn");
+  }
+}
+
+function check(array, userInputVal, checkItem) {
+  for (let i = 0; i < array.length; i++) {
+    if (userInputVal.value.toLowerCase() == array[i].checkItem.toLowerCase()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- main.js
+++ main.js
@@ -7,16 +7,21 @@ document.addEventListener("click", clickHandler);
 function clickHandler() {
   if (event.target.nodeName == "BUTTON") {
     if (event.target.id == "loginBtn") {
-      document.getElementById("logIn").style.display = "none";
-      document.getElementById("managePass").style.display = "block";
+      clearDivs();
+      displayDiv("managePass");
       document.body.classList.add('bg2');
       document.body.classList.remove('bg1');
+
+
     } else if  (event.target.id == "logOut") {
-      document.getElementById("logIn").style.display = "block";
-      document.getElementById("managePass").style.display = "none";
-      existAcc();
+      clearDivs();
+      displayDiv("logIn")
       document.body.classList.add('bg1');
       document.body.classList.remove('bg2');
+
+    } else if (event.target.id == "createAccBtn") {
+      createLogin();
     }
   }
 }
+
